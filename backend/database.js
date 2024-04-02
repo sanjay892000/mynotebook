2a0482b736bdb8737916a7ae9bcd66ddc20f0c60
sanjay892000 <sanjay892000@gmail.com>
2024-04-02 19:45:52 +0530

    dotenv

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,12 @@
 const { mongoose } = require("mongoose");
 require('dotenv').config();
-const mongoURI="mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.0" || process.env.DB_URI;
- const connectToMongo=()=>{
+const mongoURI = process.env.DB_ATLUS;
+const connectToMongo = () => {
   mongoose.connect(mongoURI)
-  .then(() => {
-    console.log("database connected")
-  }).catch((err) => {
-    console.log("something went wrong")
-  });
- }
-  module.exports=connectToMongo;
+    .then(() => {
+      console.log("database connected")
+    }).catch((err) => {
+      console.log("something went wrong")
+    });
+}
+module.exports = connectToMongo;
