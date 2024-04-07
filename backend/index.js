cf5db2fa93c773f54d33d64aad5f9fd880e3bf41
sanjay892000 <sanjay892000@gmail.com>
2024-04-07 14:29:50 +0530

    cors change

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT_BACK || 5000
 const app = express()
 
 app.use(cors({
-  origin:`http://localhost:5000`,
+  origin:`http://localhost:3000`,
     methods:"GET,POST,PUT,DELETE",
     credentials:true}
 ));
